0ca202aea25c9ecb8b8dcaf71627b0287d3fb100
Gabriel Marques <gabrielmarques409@gmail.com>
2023-06-13 21:21:59 -0300

    ajustes no script

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,29 @@
-fetch(`https://rickandmortyapi.com/api/character`)
-.then( persons =>  persons.json())
-.then( persons => {
-    persons.forEach(person => {
-        const main = document.querySelector(".persons")
-        const box = document.createElement("div")
-        main.appendChild(box)
-        const img = document.createElement("img")
-        img.setAttribute("src", person.image)
-        box.appendChild(img)
-        const name = document.createElement("h1")
-        name.textContent = person.name
-        box.appendChild(name)
-        const place = document.createElement("h2")
-        place.textContent = person.origin.name
-        box.appendChild(place)
+const personsContanier = document.querySelector(".persons")
+
+async function getPersons (){
+    const response = await fetch(`https://rickandmortyapi.com/api/character`)
+
+    const persons = await response.json
+    return persons.results
+}
+
+async function render ({persons}){
+    persons.forEach((person) => {
+
+        return personsContanier.innerHTML += `
+        <div>
+            <img src="${person.image}" alt="">
+            <div>
+            <h2>${person.name}</h2>
+            <h3>${person.origin}</h3>
+            </div>
+        </div>`
     });
-})
+}   
+
+async function main (){
+    const persons = await getPersons()
+    render(persons)
+}
+
+main()
